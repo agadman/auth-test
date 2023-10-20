6afe3530e4843c8858a797921d54a84ae88766d2
Annika Gadman <annika.gadman@gmail.com>
2023-10-20 08:51:19 -0500

    start

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './app/screens/Login';
+import List from './app/screens/List';
+import Details from './app/screens/Details';
+import React, { useEffect, useState} from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { FIREBASE_AUTH } from './FirebaseConfig';
+
+const Stack = createNativeStackNavigator();
+
+const InsideStack = createNativeStackNavigator();
+
+function InsideLayout() {
+  return (
+    <InsideStack.Navigator>
+      <InsideStack.Screen title="My todos" component={List} />
+      <InsideStack.Screen title="details" component={Details} />
+    </InsideStack.Navigator>
+  )
+}
 
 export default function App() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      console.log('user', user);
+      setUser(user);
+    })
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        {user ? (
+          <Stack.Screen name='Inside' component={InsideLayout} options={{ headerShown: false }}></Stack.Screen>
+        ) : (
+          <Stack.Screen name='Login' component={Login} options={{ headerShown: false }}></Stack.Screen>
+        )}    
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ FirebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqy95Bqv4e4kljz10G5_tKIlUWG1U2ogY",
+  authDomain: "prototypetest-4ba57.firebaseapp.com",
+  projectId: "prototypetest-4ba57",
+  storageBucket: "prototypetest-4ba57.appspot.com",
+  messagingSenderId: "498812493991",
+  appId: "1:498812493991:web:8b4dbfc9f1fb6aa5933b82"
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
