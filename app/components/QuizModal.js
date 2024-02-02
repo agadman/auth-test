b303f0a410d924b799087ea0145201f9981b5072
Annika Gadman <annika.gadman@gmail.com>
2024-02-02 09:57:51 -0600

    added areaMapping to quiz and show result

--- app/components/QuizModal.js
+++ app/components/QuizModal.js
@@ -1,23 +1,94 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, ScrollView, StyleSheet, Modal } from 'react-native';
 import { COLORS } from '../components/Colors';
+import areaMapping from '../../areaMapping.json';
 
 const QuizModal = ({ isVisible, onClose, questions = [] }) => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [quizStarted, setQuizStarted] = useState(false);
+  const [selectedAnswers, setSelectedAnswers] = useState([]); // Use an array for multiple answers
+  const [quizCompleted, setQuizCompleted] = useState(false);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [resultAreas, setResultAreas] = useState({});
+
+  useEffect(() => {
+    // Reset the quiz state when the modal is opened
+    if (isVisible) {
+      setCurrentQuestionIndex(0);
+      setQuizStarted(false);
+      setSelectedAnswers([]);
+      setQuizCompleted(false);
+      setUserAnswers([]);
+      setResultAreas({});
+    }
+  }, [isVisible]);
 
   const handleStartQuiz = () => {
     setQuizStarted(true);
   };
 
   const handleNextQuestion = () => {
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-    } else {
-      // Handle end of the quiz, you can close the modal or show a summary
-      onClose();
-      setQuizStarted(false);
+    if (selectedAnswers.length > 0) {
+      const currentQuestion = questions[currentQuestionIndex];
+  
+      const areas = Object.keys(areaMapping);
+      for (const area of areas) {
+        const keywords = areaMapping[area];
+        if (selectedAnswers.some(answer => keywords.some(keyword => answer.toLowerCase().includes(keyword.toLowerCase())))) {
+          setResultAreas({ ...resultAreas, [area]: (resultAreas[area] || 0) + 1 });
+          break; // Stop checking other areas once a match is found
+        }
+      }
+  
+      // Proceed to the next question only if at least one answer is selected
+      if (currentQuestionIndex < questions.length - 1) {
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+        setUserAnswers([...userAnswers, selectedAnswers]); // Save the selected answers
+        setSelectedAnswers([]); // Reset selected answers for the next question
+      } else {
+        // Handle end of the quiz, show summary
+        setUserAnswers([...userAnswers, selectedAnswers]); // Save the last selected answers
+        setQuizCompleted(true);
+      }
     }
+  };
+
+
+  const handleAnswerSelect = (answer) => {
+    const currentQuestion = questions[currentQuestionIndex];
+    
+    // Toggle the selection for multiple answers if allowed
+    if (currentQuestion.allowMultipleAnswers) {
+      setSelectedAnswers((prevSelectedAnswers) => {
+        if (prevSelectedAnswers.includes(answer)) {
+          return prevSelectedAnswers.filter((selectedAnswer) => selectedAnswer !== answer);
+        } else {
+          return [...prevSelectedAnswers, answer];
+        }
+      });
+    } else {
+      // Single answer mode
+      setSelectedAnswers([answer]);
+    }
+  };
+
+  const getMaxArea = () => {
+    let maxArea = null;
+    let maxCount = 0;
+
+    for (const area in resultAreas) {
+      const count = resultAreas[area];
+      if (count > maxCount) {
+        maxCount = count;
+        maxArea = area;
+      }
+    }
+
+    return maxArea;
+  };
+
+  const handleEndQuiz = () => {
+    onClose(); 
   };
 
   return (
@@ -32,17 +103,15 @@ const QuizModal = ({ isVisible, onClose, questions = [] }) => {
       }}
     >
       <View style={styles.container}>
-          <Text style={styles.closeX} onPress={onClose}>
-            X
-          </Text>
+        <Text style={styles.closeX} onPress={onClose}>
+          X
+        </Text>
 
-          <ScrollView style={styles.content}>
+        <ScrollView style={styles.content}>
           {/* Display the initial message and Start Quiz button */}
-          {!quizStarted && (
+          {!quizStarted && !quizCompleted && (
             <View>
-              <Text style={styles.initialMessage}>
-                Ok, nu kör vi...
-              </Text>
+              <Text style={styles.initialMessage}>Ok, nu kör vi...</Text>
               <TouchableOpacity onPress={handleStartQuiz}>
                 <Text style={styles.startQuizButton}>Start Quiz</Text>
               </TouchableOpacity>
@@ -50,7 +119,7 @@ const QuizModal = ({ isVisible, onClose, questions = [] }) => {
           )}
 
           {/* Display the current question */}
-          {quizStarted && (
+          {quizStarted && !quizCompleted && (
             <>
               <Text style={styles.questionText}>
                 {questions[currentQuestionIndex]?.question}
@@ -58,18 +127,48 @@ const QuizModal = ({ isVisible, onClose, questions = [] }) => {
 
               {/* Display the answers for the current question */}
               {questions[currentQuestionIndex]?.answers.map((answer, index) => (
-                <TouchableOpacity key={index} style={styles.answerButton}>
+                <TouchableOpacity
+                  key={index}
+                  style={[
+                    styles.answerButton,
+                    selectedAnswers.includes(answer) && styles.selectedAnswerButton,
+                  ]}
+                  onPress={() => handleAnswerSelect(answer)}
+                >
                   <Text>{answer}</Text>
                 </TouchableOpacity>
               ))}
-              
+
               {/* Display Next button */}
-              <TouchableOpacity onPress={handleNextQuestion}>
-                <Text style={styles.nextButton}>Next</Text>
+              <TouchableOpacity
+                onPress={handleNextQuestion}
+                disabled={selectedAnswers.length === 0} // Disable button if no answer is selected
+              >
+                <Text
+                  style={[
+                    styles.nextButton,
+                    selectedAnswers.length === 0 && styles.disabledButton,
+                  ]}
+                >
+                  Next
+                </Text>
               </TouchableOpacity>
             </>
           )}
-        </ScrollView>   
+
+          {/* Display quiz summary */}
+          {quizCompleted && (
+            <>
+              <Text style={styles.summaryText}>Quiz Summary</Text>
+              <View style={styles.summaryItem}>
+                <Text>{`Your area is: ${getMaxArea()}`}</Text>
+              </View>
+              <TouchableOpacity onPress={handleEndQuiz}>
+                <Text style={styles.nextButton}>End Quiz</Text>
+              </TouchableOpacity>
+            </>
+          )}
+        </ScrollView>
       </View>
     </Modal>
   );
@@ -126,6 +225,28 @@ const styles = StyleSheet.create({
     padding: 20,
     width: '75%',
     marginTop: 20,
+  },
+
+  selectedAnswerButton: {
+    backgroundColor: '#AED581', // You can customize the color for the selected answer
+  },
+
+  disabledButton: {
+    backgroundColor: '#D3D3D3', // You can customize the color for the disabled button
+  },
+
+  summaryText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+
+  summaryItem: {
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: '#EFEFEF',
+    borderRadius: 5,
   },
 });
 
